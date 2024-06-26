ef2d579f1ae134dc3d7354c15ec9c4801961a8ed
Levi Zitting <me@levizitting.com>
2024-06-26 09:01:17 -0500

    feat: add hello endpoint

---
+++ OpenTelemetryWorkshop/Controllers/HelloController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenTelemetryWorkshop.Controllers;
+
+[ApiController]
+[Route("hello")]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<string> GetHello([FromQuery] string firstName, [FromQuery] string surname)
+    {
+        return $"Hello {firstName} {surname}";
+    }
+}
