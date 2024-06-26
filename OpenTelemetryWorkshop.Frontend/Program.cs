aed743c4080702ea269b4b0f535c2e8ba02bd0c0
Levi Zitting <me@levizitting.com>
2024-06-26 11:17:58 -0500

    feat: add http client instrumentation

--- OpenTelemetryWorkshop.Frontend/Program.cs
+++ OpenTelemetryWorkshop.Frontend/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddOpenTelemetry()
     .WithTracing(tpb =>
         tpb
             .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
             .AddConsoleExporter()
             .AddOtlpExporter());
 
